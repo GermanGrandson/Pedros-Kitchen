ab86368c916db623026a16f707d59dc5112c5a61
German Grandson <ganieto1@gmail.com>
2016-04-13 17:08:50 -0400

    bootstrap and paperclip functionality

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -45,7 +45,7 @@ before_action :find_recipe, only: [:show, :edit, :update, :destroy]
   end
 
   def recipe_params
-    params.require(:recipe).permit(:title, :description)
+    params.require(:recipe).permit(:title, :description, :image)
   end
 
 end
